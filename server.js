eb6046d28ffb2fc6aa53747bfae2ab7faba85a0e
Ola Faxe <olafaxe@gmail.com>
2020-05-12 16:54:56 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8000;
+
+const MongoClient = require("mongodb").MongoClient;
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let db;
+
+client.connect(function (err) {
+  if (err) throw err;
+  //users is the name of the collection from the db document
+  db = client.db("flowez");
+});
+
+//getting all the users from document db "fake", collection "users"
+app.get("/flowcharts", (req, res) => {
+  const usersCollection = db.collection("flowcharts");
+  usersCollection.find({}).toArray(function (err, users) {
+    res.send(users);
+  });
+});
+
+//parsing incoming requests
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+//use static files
+// app.use(express.static("views/public"));
+
+//simple get rout with variable sent to "/" for use at pages/index
+app.get("/", (req, res) => {
+  res.send("hej");
+});
+
+app.listen(port, () => {
+  console.log(`Lyssnar på port: ${port}`);
+});
