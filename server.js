cfd805b3cbe463deaff34c4fb2ae22c06a3b9cc1
Ola Faxe <olafaxe@gmail.com>
2020-06-04 21:07:34 +0200

    env heroku var

--- server.js
+++ server.js
@@ -18,11 +18,9 @@ let db;
 
 client.connect(function (err) {
   if (err) throw err;
-  //users is the name of the collection from the db document
-  db = client.db("heroku_d8xww5mw");
+  db = client.db(process.env.HEROKU_URL);
 });
 
-//getting all the users from document db "fake", collection "users"
 app.get("/flowcharts", (req, res) => {
   const flowchartCollection = db.collection("flowcharts");
   flowchartCollection.find({}).toArray(function (err, charts) {
@@ -45,52 +43,12 @@ app.delete("/flowcharts/:id", (req, res) => {
     .then(() => res.send(req.params.id));
 });
 
-// app.put("/todos/:id", (req, res) => {
-//   //put
-//   const todo = req.body;
-//   const todoCollection = db.collection("todos");
-//   todoCollection
-//     .replaceOne(
-//       { id: Number(req.params.id) },
-//       {
-//         id: todo.id,
-//         filter: todo.filter,
-//         checked: todo.checked,
-//         edit: todo.edit,
-//         delete: todo.delete,
-//         content: todo.content,
-//         date: todo.date
-//       }
-//     )
-//     .then(() => res.send(todo));
-// });
-
-// app.patch("/todos/:id", (req, res) => {
-//   const checked = req.body.checked;
-//   const todoCollection = db.collection("todos");
-//   todoCollection
-//     .updateOne(
-//       { id: Number(req.params.id) },
-//       {
-//         $set: {
-//           checked: checked
-//         }
-//       }
-//     )
-//     .then(() => res.send(checked));
-// });
-
-//parsing incoming requests
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
-//use static files
-// app.use(express.static("views/public"));
-
-//simple get rout with variable sent to "/" for use at pages/index
 app.get("/", (req, res) => {
   res.send("working");
 });
